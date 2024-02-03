4642461b718a4b24be9b819fca424c1fdee4266e
Nonie <137665882+noniesatan@users.noreply.github.com>
2024-02-03 21:30:25 +1100

    Update V2 3Feb24

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Menu.css';
 
 function Menu() {
+
+        const [isMenuOpen, setIsMenuOpen] = useState(false);
+        const toggleMenu = () => {
+                setIsMenuOpen(!isMenuOpen)
+        }
+
+
   return (
     <nav className='bg-transparent p-4 font-sans'>
         <div className='flex items-center justify-between'>
@@ -9,7 +16,7 @@ function Menu() {
             <div className='text-2xl font-bold text-black font-sans ml-2'>Ahka Siri</div>
                 {/* Togglo */}
                 <div className='md:hidden'>
-                    <button id='menu-toggle' className='text-white'>
+                    <button id='menu-toggle' onClick={toggleMenu} className='text-white'>
                         <svg fill='none' 
                         stroke='currentColor' 
                         strokeLinecap='round' 
@@ -36,6 +43,18 @@ function Menu() {
 
             </ul>
         </div>
+        {/* Mobile menu */}
+        {isMenuOpen ? (
+                 <ul className='flex-col md:hidden mr-2'>
+                 <li><a href="https://www.youtube.com/" className='text-orange-200 link link-hover py-3' id='linav'>Home</a></li>
+                     <li><a href="/about" className='text-orange-200'>About</a></li>
+                     <li><a href="/reservation" className='text-orange-200'>Reservation</a></li>
+                     <li><a href="/contact" className='text-orange-200'>Contact</a></li>
+     
+                 </ul>
+
+        ) : null }
+       
 
     </nav>
    
